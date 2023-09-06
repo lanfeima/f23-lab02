ca7c7fb9ac8bfa8beab1ee858c5315a85d9b0de0
Lanfei Ma <lma87@wisc.edu>
2023-09-06 15:18:38 -0400

    change java and typescript

--- java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Circle.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Circle.java
@@ -1,12 +1,21 @@
 package edu.cmu.cs.cs214.lab02.shapes;
 
 public class Circle implements Shape {
-    public double radius;
+    private double radius;
 
     public Circle(double radius) {
         this.radius = radius;
     }
 
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
     public double getArea() {
         return Math.PI * radius * radius;
     }
--- typescript/src/shapes/rectangle.ts
+++ typescript/src/shapes/rectangle.ts
@@ -1,18 +1,12 @@
-interface Rectangle {
-    width: number,
-    height: number,
-    computeArea: () => number
-}
+import { Shape } from "./shape.js";
 
-function newRectangle(width: number, height: number): Rectangle {
+function newRectangle(width: number, height: number): Shape {
     return {
-        width,
-        height,
-
-        computeArea: function (): number {
-            return width * height
-        }
-    }
-}
-
-export { Rectangle, newRectangle }
+      computeArea: function (): number {
+        return width * height;
+      }
+    };
+  }
+  
+  export { newRectangle };
+  
